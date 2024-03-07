0fd6d9cd24857bcbd0c35d117c16f355d92fed72
Syed Saud Ali <160472981+SyedSaudAli@users.noreply.github.com>
2024-03-07 17:35:51 +0500

    Add files via upload

---
+++ app_v3.ts
@@ -0,0 +1,13 @@
+let alienColors : string = 'red';
+
+if( alienColors === 'green'){
+    console.log('player earned 5 points.')
+}
+
+else if (alienColors === 'yellow'){
+    console.log('player earned 10 points.')
+}
+
+else if (alienColors === 'red'){
+    console.log('player earned 15 points.')
+}
